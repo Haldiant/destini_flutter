e623d3a7cc772944c9d22050f6703773a12bda0d
Haldiant <nhalwidianto@gmail.com>
2024-07-24 14:33:04 +0700

    preparation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:destini_flutter/story.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyDestini());
@@ -10,6 +14,88 @@ class MyDestini extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp(
+      home: DestiniStory(),
+    );
+  }
+}
+
+class DestiniStory extends StatefulWidget {
+  const DestiniStory({super.key});
+
+  @override
+  State<DestiniStory> createState() => _DestiniStoryState();
+}
+
+class _DestiniStoryState extends State<DestiniStory> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 50.0, horizontal: 50.0),
+        constraints: const BoxConstraints.expand(),
+        child: const SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                flex: 10,
+                child: Center(
+                  child: Text(
+                    "Cerita ditampilkan disini",
+                    style: CustomTextStyle.headOfAppText,
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: TextButton(
+                  onPressed: null,
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.red),
+                    shape: MaterialStatePropertyAll(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.zero),
+                      ),
+                    ),
+                  ),
+                  child: Text(
+                    "Tombol 1",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Expanded(
+                flex: 2,
+                child: TextButton(
+                  onPressed: null,
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.blue),
+                    shape: MaterialStatePropertyAll(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.zero),
+                      ),
+                    ),
+                  ),
+                  child: Text(
+                    "Tombol 2",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
